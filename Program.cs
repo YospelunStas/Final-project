2da04e55c39cd6d81dbb78ef2f0c6f96ca4a9b24
Stanislav <stas2001.nikitin@yandex.ru>
2023-03-12 23:52:48 +0400

    Создали массив

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше либо равна 3.
+
+string[] s1 = new string[5] {"ru", "hello", "777", "A", "1234"};
