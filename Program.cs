231f7520fdfd154dafd24d995bf8616ff1e7a104
Stanislav <stas2001.nikitin@yandex.ru>
2023-03-13 00:01:05 +0400

    Добавили метод по выводу массива

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿// Напишите программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше либо равна 3.
 
+void PrintArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
 string[] s1 = new string[5] {"ru", "hello", "777", "A", "1234"};
